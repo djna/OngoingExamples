dda24d1130484edcdaf24c99627fcf8f74050c41
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2022-04-02 17:15:57 +0100

    Register - a list and hashmap example

--- Lambda/Citizen/src/org/djna/citizen/Register.java
+++ Lambda/Citizen/src/org/djna/citizen/Register.java
@@ -5,48 +5,57 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+// Contrived example using collections to keep some objects
+
 public class Register {
 
+    // list of people eligible to vote
     static private List<Person>voters = new ArrayList<>();
+
+    // record of when a voter is added to the elctoral role
     static private Map<Person, VoterRegistration> electoralRoll = new HashMap<>();
 
     public static void main(String[] args) {
 
-        Person abe = new Person("Abe", 23);
-        Person bakerSenior  = new Person("Baker", 70);
-        Person bakerJunior  = new Person("Baker", 45);
-        Person bakerTheThird  = new Person("Baker", 19);
-        Person charlie =  new Person("Charlie", 83);
-
-        voters.add( abe );
-        voters.add( bakerSenior);
-        voters.add( bakerJunior);
-        voters.add( bakerTheThird);
-        voters.add( charlie);
+        registerVoter("Abe", 23);
+        registerVoter("Baker", 70);
+        registerVoter("Baker", 45);
+        registerVoter("Baker", 19);
+        registerVoter("Charlie", 83);
 
         System.out.printf("List of people:\n%s\n\n", voters);
 
-        registerVoter(bakerJunior);
-        registerVoter(bakerSenior);
-        registerVoter(bakerTheThird);
-
-        System.out.printf("Baker junior: %s : %b\n", bakerJunior, voters.contains(bakerJunior));
 
         printVoter("Abe", 23);
         printVoter("Baker", 45);
 
+        printElectoralRollEntry("Baker", 45);
+
     }
 
+    static void printAllVoters(){
+        System.out.printf("List of registered voters:\n%s\n\n", voters);
+    }
+
+    // print whether a voter has been registered
     static void printVoter( String name, int age){
         Person voter = new Person(name, age);
 
-        System.out.printf("%s : %b\n", voter, voters.contains(voter));
-
-
-
+        System.out.printf("%s : %s\n", voter, voters.contains(voter) ? "is regiestered " : "unknown, not in list of registered voters");
     }
 
-    static void registerVoter(Person voter){
+    // look for an electoral roll entry for a voter
+    static void printElectoralRollEntry( String name, int age){
+        Person voter = new Person(name, age);
+        VoterRegistration registration = electoralRoll.get(voter);
+        System.out.printf("%s : %s\n", voter, registration  ==  null
+                                              ? "not on roll " : "registered on " + registration);
+    }
+
+    // create a voter record and add to electoral roll
+    static void registerVoter(String name, int age){
+        Person voter = new Person(name, age);
+        voters.add( voter );
         VoterRegistration registration = new VoterRegistration();
         electoralRoll.put(voter, registration);
     }
