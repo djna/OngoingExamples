b99bc24fe39e865655f91783583439d7475c5cf3
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2022-04-04 15:29:36 +0100

    simple stream examples

---
+++ Streams/src/org/djna/streams/Builder.java
@@ -0,0 +1,40 @@
+package org.djna.streams;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Builder {
+    private static int x = 0;
+    static int makeInt(){
+        System.out.printf("get %d%n", x);
+        return x++;
+    }
+    public static void main(String[] args) {
+
+     /*   File file = new File("words.txt");
+        Map<String, Long> freq = new HashMap<>();
+        try (Stream<String> words = new Scanner(file).tokens()) {
+            words.forEach(word -> {
+                freq.merge(word.toLowerCase(), 1L, Long::sum);
+            });
+        } catch (Exception e) {
+            System.out.printf("exception %s%n", e);
+        }
+        System.out.printf("Freq %s%n", freq);*/
+
+
+        Stream<Integer> infiniteIntegers = Stream.generate(() -> makeInt() );
+        long howMany = infiniteIntegers.limit(10).count();
+        System.out.printf("got %d%n", howMany);
+
+        infiniteIntegers.max(Integer::compare);
+
+
+
+
+    }
+}
+
