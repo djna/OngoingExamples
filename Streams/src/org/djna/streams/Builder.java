8e486873d0842d5af999ccd0b155d74c3b94dbf1
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2022-04-05 09:05:29 +0100

    style example

--- Streams/src/org/djna/streams/Builder.java
+++ Streams/src/org/djna/streams/Builder.java
@@ -5,16 +5,19 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Stream;
+import static java.util.stream.Collectors.*;
+
 
 public class Builder {
-    private static int x = 0;
-    static int makeInt(){
-        System.out.printf("get %d%n", x);
-        return x++;
-    }
-    public static void main(String[] args) {
 
-     /*   File file = new File("words.txt");
+    public static void main(String[] args) {
+        badStyle();
+        goodStyle();
+    }
+
+    private static void badStyle(){
+
+        File file = new File("words.txt");
         Map<String, Long> freq = new HashMap<>();
         try (Stream<String> words = new Scanner(file).tokens()) {
             words.forEach(word -> {
@@ -23,16 +26,22 @@ public class Builder {
         } catch (Exception e) {
             System.out.printf("exception %s%n", e);
         }
-        System.out.printf("Freq %s%n", freq);*/
+        System.out.printf("Freq %s%n", freq);
 
+    }
 
-        Stream<Integer> infiniteIntegers = Stream.generate(() -> makeInt() );
-        long howMany = infiniteIntegers.limit(10).count();
-        System.out.printf("got %d%n", howMany);
+    private static void goodStyle(){
 
-        infiniteIntegers.max(Integer::compare);
-
-
+        File file = new File("words.txt");
+        Map<String, Long> freq;
+        try (Stream<String> words = new Scanner(file).tokens()) {
+            freq = words.collect(
+                    groupingBy( String::toLowerCase, java.util.stream.Collectors.counting() )
+            );
+            System.out.printf("Freq %s%n", freq);
+        } catch (Exception e) {
+            System.out.printf("exception %s%n", e);
+        }
 
 
     }
