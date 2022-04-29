96ffbd492afe97e43de89e142fe76a414101162b
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2022-04-29 10:10:02 +0100

    Join Threads

--- Threads/src/main/java/org/djna/examples/RunnableExample.java
+++ Threads/src/main/java/org/djna/examples/RunnableExample.java
@@ -1,13 +1,18 @@
 package org.djna.examples;
+
+import java.util.Random;
+
 public class RunnableExample {
+
 
     public static void main(String[] args) {
 
         Runnable longRunningCalculation = () -> {
 
             try {
-                Thread.sleep(500);
-                System.out.printf("Ping %d%n", Thread.currentThread().getId());
+                long sleepFor = new Random().nextInt(5000) + 2000L;
+                Thread.sleep(sleepFor);
+                System.out.printf("Ping %d after %d%n", Thread.currentThread().getId(), sleepFor);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -16,8 +21,17 @@ public class RunnableExample {
         System.out.printf("Main in %d%n", Thread.currentThread().getId());
         Thread backgroundCalculationThread1 = new Thread(longRunningCalculation);
         Thread backgroundCalculationThread2 = new Thread(longRunningCalculation);
-        backgroundCalculationThread1.run();
         backgroundCalculationThread1.start();
         backgroundCalculationThread2.start();
+        try {
+            System.out.printf("Wait for 1%n");
+            backgroundCalculationThread1.join();
+            System.out.printf("Joined 1%n");
+            backgroundCalculationThread2.join();
+            System.out.printf("Joined 2%n");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
