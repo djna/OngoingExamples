cda48870fadc25c7691a907c2ce33e1fd1c4f138
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2022-04-29 09:53:47 +0100

    Demonstrate run() v start()

--- Threads/src/main/java/org/djna/examples/RunnableExample.java
+++ Threads/src/main/java/org/djna/examples/RunnableExample.java
@@ -13,8 +13,10 @@ public class RunnableExample {
             }
         };
 
+        System.out.printf("Main in %d%n", Thread.currentThread().getId());
         Thread backgroundCalculationThread1 = new Thread(longRunningCalculation);
         Thread backgroundCalculationThread2 = new Thread(longRunningCalculation);
+        backgroundCalculationThread1.run();
         backgroundCalculationThread1.start();
         backgroundCalculationThread2.start();
     }
