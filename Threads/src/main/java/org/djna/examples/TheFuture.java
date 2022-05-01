ae05e1773b169f4dc7fe1fa424bed69f7f57af62
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2022-05-01 11:52:24 +0100

    variations on acceptAsync

--- Threads/src/main/java/org/djna/examples/TheFuture.java
+++ Threads/src/main/java/org/djna/examples/TheFuture.java
@@ -8,9 +8,49 @@ import java.util.concurrent.Future;
 public class TheFuture {
     static ExecutorService threadPool = Executors.newCachedThreadPool();
 
+    public static void main(String[] args) throws Exception {
+
+        TheFuture oneFuture = new TheFuture();
+        Future<String> future = oneFuture.calculateAsync();
+        System.out.printf("get result in %s ... %n", Thread.currentThread().getName());
+        String result = future.get();
+        System.out.printf("Future %s in %s%n", result, Thread.currentThread().getName());
+
+        Future<String> instantFuture =
+                CompletableFuture.completedFuture("Hello instantly");
+
+        System.out.printf("get instant result in %s ... %n", Thread.currentThread().getName());
+        System.out.printf("Future %s%n", instantFuture.get());
+
+        CompletableFuture<String> asyncFuture =
+                CompletableFuture.supplyAsync(
+                        () -> {
+                            System.out.println("async a bit ...");
+                            try {
+                                Thread.sleep(5000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            String value = "HelloAsynch";
+                            System.out.printf("returning on %s with %s%n",
+                                    Thread.currentThread().getName(),
+                                    value
+                            );
+                            return value;
+                        }
+                );
+
+        System.out.printf("AsyncFuture %s%n", asyncFuture.get());
+
+        asyncFuture.thenAccept( s -> {
+            System.out.printf("accept AsyncFuture %s%n", s);
+        });
+
+    }
+
     public Future<String> calculateAsync() throws InterruptedException {
         CompletableFuture<String> completableFuture = new CompletableFuture<>();
-        
+
         threadPool.submit(() -> {
             System.out.println("think a bit ...");
             Thread.sleep(5000);
@@ -18,21 +58,11 @@ public class TheFuture {
             System.out.printf("Completing on %s with %s%n",
                     Thread.currentThread().getName(),
                     value
-                    );
+            );
             completableFuture.complete("Hello");
             return null;
         });
 
         return completableFuture;
-    }
-
-    public static void main(String[] args) throws Exception {
-
-        TheFuture oneFuture = new TheFuture();
-        Future<String> future = oneFuture.calculateAsync();
-        System.out.printf("get result in %s ... %n", Thread.currentThread().getName());
-        String result = future.get();
-        System.out.printf("Future %s in %s%n", result, Thread.currentThread().getName() );
-
     }
 }
